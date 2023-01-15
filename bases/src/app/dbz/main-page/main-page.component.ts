1e6d86d55834e312dfdb02dd0228c19f8139b7bc
Alex Yovani <alexyovani53@gmail.com>
2023-01-15 11:32:16 -0600

    Forms sin NgModel

--- bases/src/app/dbz/main-page/main-page.component.ts
+++ bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string
+  poder: number
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
@@ -7,13 +12,24 @@ import { Component } from '@angular/core';
 })
 export class MainPageComponent {
 
-
+  nuevo:Personaje = {
+    nombre: "Trunks", 
+    poder: 14000
+  }
   // agregar(event: any){
   //   event.preventDefault()
   //   console.log(event)
   // }
 
   agregar(){
-    console.log("prueba")
+    console.log(this.nuevo)
+  }
+
+  cambiarNombre(event: any){
+    this.nuevo.nombre = event.target.value
+  }
+
+  cambiarPoder(event: any){
+    this.nuevo.poder = event.target.value
   }
 }
