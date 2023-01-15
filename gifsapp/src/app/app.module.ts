c052677dc5e789a84346958e46df4db6e9cca211
Alex Yovani <alexyovani53@gmail.com>
2023-01-15 15:03:29 -0600

    Módulo Shared

--- gifsapp/src/app/app.module.ts
+++ gifsapp/src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ShareModule } from './share/share.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    ShareModule
   ],
   providers: [],
   bootstrap: [AppComponent]
