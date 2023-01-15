c79e072e9fdec4c9cd64723a42cb748b862f6691
Alex Yovani <alexyovani53@gmail.com>
2023-01-15 15:13:42 -0600

    GifsModule y sus componentes

--- gifsapp/src/app/app.module.ts
+++ gifsapp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GifsModule } from './gifs/gifs.module';
 import { ShareModule } from './share/share.module';
 
 @NgModule({
@@ -10,7 +11,8 @@ import { ShareModule } from './share/share.module';
   ],
   imports: [
     BrowserModule, 
-    ShareModule
+    ShareModule, 
+    GifsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
