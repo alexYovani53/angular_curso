9733de75b37336f3eb46768fabf9594290207ec4
Alex Yovani <alexyovani53@gmail.com>
2023-01-19 15:45:15 -0600

    Componente para buscar por pais

--- paisesApp/src/app/pais/pais.module.ts
+++ paisesApp/src/app/pais/pais.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { PorCapitalComponent } from './pages/por-capital/por-capital.component';
 import { PorPaisComponent } from './pages/por-pais/por-pais.component';
 import { PorRegionComponent } from './pages/por-region/por-region.component';
@@ -14,14 +15,15 @@ import { VerPaisComponent } from './pages/ver-pais/ver-pais.component';
     PorRegionComponent,
     VerPaisComponent
   ],
-  exports:[
+  exports: [
     PorCapitalComponent,
     PorPaisComponent,
     PorRegionComponent,
-    VerPaisComponent
-  ], 
+    VerPaisComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class PaisModule { }
