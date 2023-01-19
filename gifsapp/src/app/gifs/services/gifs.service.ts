badc54a1831eb73329de8172c038e0f822cc67e4
Alex Yovani <alexyovani53@gmail.com>
2023-01-19 13:05:31 -0600

    Guardar historial en localStorage

--- gifsapp/src/app/gifs/services/gifs.service.ts
+++ gifsapp/src/app/gifs/services/gifs.service.ts
@@ -17,7 +17,9 @@ export class GifsService {
   }
 
   constructor(private http: HttpClient) {
-    
+    if (localStorage.getItem("historial")) {
+      this._historial = JSON.parse(localStorage.getItem("historial")!)
+    }
   }
   
   buscarGifs(query: string){
@@ -25,6 +27,8 @@ export class GifsService {
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.slice(0, 10)
+
+      localStorage.setItem("historial", JSON.stringify(this._historial))
     }
 
     console.log(this._historial)
