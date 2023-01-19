30b60e602c9d57f4e3791148e17d0e35219e54a9
Alex Yovani <alexyovani53@gmail.com>
2023-01-19 15:12:08 -0600

    RotuerModule - Rutas

--- paisesApp/src/app/app.module.ts
+++ paisesApp/src/app/app.module.ts
@@ -4,13 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { PaisModule } from './pais/pais.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule, 
+    BrowserModule,
+    AppRoutingModule, 
     SharedModule, 
     PaisModule
   ],
