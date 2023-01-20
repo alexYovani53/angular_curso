c6419ae5aae52f2e91c0ae11b73321e85f4a587d
Alex Yovani <alexyovani53@gmail.com>
2023-01-19 18:46:40 -0600

    Metodo para obtener css condicional

--- paisesApp/src/app/pais/pages/por-region/por-region.component.ts
+++ paisesApp/src/app/pais/pages/por-region/por-region.component.ts
@@ -9,8 +9,27 @@ import { Component } from '@angular/core';
 export class PorRegionComponent {
 
   regiones : string[] = ["Africa", "Americas", "Asia", "Europe", "Oceania"]
+  regiones2 : string[] = [
+    "EU",
+    "EFTA",
+    "CARICOM",
+    "PA",
+    "AU",
+    "USAN",
+    "EEU",
+    "AL",
+    "ASEAN",
+    "CAIS",
+    "CEFTA",
+    "NAFTA",
+    "SAARC", 
+  ]
 
   regionActiva : string = ""
+
+  getClassCSS(region:string) : string {
+    return (region !==this.regionActiva) ? 'btn btn-outline-primary' :'btn btn-primary'
+  }
 
   activar(region:string){
     this.regionActiva = region
