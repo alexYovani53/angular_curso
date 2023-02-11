e10b0193bc104b5cb2a5782bd1081aec4fb630e3
Alex Yovani <alexyovani53@gmail.com>
2023-02-10 23:09:29 -0600

    Pipe no comunes. i18nPlural

--- 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ 04-pipesApp/src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -7,11 +7,22 @@ import { Component } from '@angular/core';
   ]
 })
 export class NoComunesComponent {
+
+  //i18nSelect
   nombre: string = "Alex";
   genero: string = "masculino";
 
   invitacinMapa = {
     "masculino": 'invitarlo',
     "femenino": 'invitarla'
+  }
+
+  //i18nPlural
+  clientes: string[] = ['Maria', 'Pedro', 'Juan'];
+  clientesMapa = {
+    '=0': 'no tenemos ningún cliente esperando.',
+    '=1': 'tenemos un cliente esperando.',
+    '=2': 'tenemos dos clientes esperando.',
+    'other': 'tenemos # clientes esperando.'
   }
 }
